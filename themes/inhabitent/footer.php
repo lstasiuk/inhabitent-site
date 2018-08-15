7b886ed48b7bac3bb49fecef23c21601b301f224
lstasiuk <leanne.stasiuk@gmail.com>
2018-08-15 16:34:56 -0700

    add footer details

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -26,8 +26,10 @@
 
 			<div class="copyright">
 			<p>Copyright © 2018 Inhabitent</p>
+			<div class="footer-logo">
+			<img src="<?php echo get_template_directory_uri()?>/images/logos/inhabitent-logo-text-dark.svg"/>
+			</div>
 
-			
 
 
 				</div><!-- .site-info -->
