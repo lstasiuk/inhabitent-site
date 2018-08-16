bfa06e04ad98189504f88c54c6352fed6f44d585
lstasiuk <leanne.stasiuk@gmail.com>
2018-08-16 16:39:49 -0700

    added code to extras

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -11,17 +11,17 @@
 					<div class="contact-info footer-list">
 					<p>Contact Info</p>
 					<ul>
-						<li>info@inhabitent.com</li>
-						<li>778-456-7891</li>
+						<li><i class="far fa-envelope"></i>info@inhabitent.com</li>
+						<li><i class="fas fa-phone"></i>778-456-7891</li>
 						<i class="fab fa-facebook-square"> </i> <i class="fab fa-twitter-square"> </i> <i class="fab fa-google-plus-g"> </i>
 					</ul> 			
 					</div>
 					<div class="business-hours footer-list">
 					<p>Business Hours</p>
 					<ul>
-						<li>Monday-Friday: 9am to 5pm</li>
-						<li>Saturday: 10am to 2pm</li>
-						<li>Sunday: Closed</li>
+						<li><strong>Monday-Friday:</strong> 9am to 5pm</li>
+						<li><strong>Saturday:</strong> 10am to 2pm</li>
+						<li><strong>Sunday:</strong> Closed</li>
 					</ul>
 </div>
 					
