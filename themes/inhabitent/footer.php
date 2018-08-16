9bc73b98ba1def9c59e106cce48a715b71740c24
lstasiuk <leanne.stasiuk@gmail.com>
2018-08-16 11:28:11 -0700

    add business hours widget

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -4,32 +4,34 @@
  *
  * @package Inhabitent_Theme
  */
-
 ?>
-			
+	
 			<footer id="colophon" class="site-footer" role="contentinfo">
-			<div class="contact-info">
-			<p>Contact Info</p>
-			<ul>
-				<li>info@inhabitent.com</li>
-				<li>778-456-7891</li>
-				<i class="fab fa-facebook-square"></i> <i class="fab fa-twitter-square"></i> <i class="fab fa-google-plus-g"></i>
-			</ul> 			
-			</div>
-			<div class="business-hours">
-			<p>Business Hours</p>
-			<ul>
-				<li>Monday-Friday: 9am to 5pm</li>
-				<li>Saturday: 10am to 2pm</li>
-				<li>Sunday: Closed</li>
-			</ul>
-
-			<div class="copyright">
-			<p>Copyright © 2018 Inhabitent</p>
-			<div class="footer-logo">
-			<img src="<?php echo get_template_directory_uri()?>/images/logos/inhabitent-logo-text-dark.svg"/>
-			</div>
-
+				<div class="footer-container">
+					<div class="contact-info footer-list">
+					<p>Contact Info</p>
+					<ul>
+						<li>info@inhabitent.com</li>
+						<li>778-456-7891</li>
+						<i class="fab fa-facebook-square"> </i> <i class="fab fa-twitter-square"> </i> <i class="fab fa-google-plus-g"></i>
+					</ul> 			
+					</div>
+					<div class="business-hours footer-list">
+					<p>Business Hours</p>
+					<ul>
+						<li>Monday-Friday: 9am to 5pm</li>
+						<li>Saturday: 10am to 2pm</li>
+						<li>Sunday: Closed</li>
+					</ul>
+</div>
+					
+					<div class="footer-logo">
+					<img src="<?php echo get_template_directory_uri()?>/images/logos/inhabitent-logo-text.svg"/>
+					</div>
+</div>
+				<div class="copyright">
+					<p>Copyright © 2018 Inhabitent</p>
+					</div>
 
 
 				</div><!-- .site-info -->
