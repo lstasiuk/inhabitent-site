f6a61b23454630a1c9f76f4f7af2eb5a657b7fa5
lstasiuk <leanne.stasiuk@gmail.com>
2018-08-19 18:34:44 -0700

    footer complete

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -6,48 +6,47 @@
  */
 ?>
 	 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="footer-container footer-list">
+			<div class="footer-container">
 				<p>Contact Info</p>
 				<ul>
 								<li>
 									<i class="fas fa-envelope"></i>
 									info@inhabitent.com
-								</li>
-								<li>
-									<i class="fas fa-phone"></i>
+ 								</li>
+							<li>
+								<i class="fas fa-phone"></i>
 									778-456-7891
 								</li>
 								<li>
-									<i class="fab fa-facebook-square"></i>
+								<i class="fab fa-facebook-square"></i>
 									<i class="fab fa-twitter-square"></i>
 									<i class="fab fa-google-plus-square"></i>
-									<i class="fab fa-instagram"></i>
-								</li>
+ 								</li>
 							</ul>
-					</div>
-					<div class="business-hours footer-list">
+ 					</div>
+ 					<div class="business-hours">
 					<p>Business Hours</p>
 					<ul>
-						<li><strong>Monday-Friday:</strong> 9am to 5pm</li>
-						<li><strong>Saturday:</strong> 10am to 2pm</li>
-						<li><strong>Sunday:</strong> Closed</li>
-					</ul>
-</div>
+ 						<li><strong>Monday-Friday:</strong> 9am to 5pm</li>
+ 						<li><strong>Saturday:</strong> 10am to 2pm</li>
+ 						<li><strong>Sunday:</strong> Closed</li>
+ 					</ul>
+ </div>
 					
 					<div class="footer-logo">
 					<img src="<?php echo get_template_directory_uri()?>/images/logos/inhabitent-logo-text.svg"/>
 					</div>
-</div>
-				<div class="copyright">
-					<p>copyright &copy; 2018 inhabitent</p>
-					</div>
+
+ 				<div class="copyright">
+ 					<p>copyright &copy; 2018 inhabitent</p>
+ 					</div>
 
 
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
-		</div><!-- #page -->
-
-		<?php wp_footer(); ?>
+		
+ 		<?php wp_footer(); ?>
 
 	</body>
 </html>
+
--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -1,40 +1,108 @@
 <?php
 /**
- * The main template file.
- * Template Name: Home Page
+ *
  * @package Inhabitent_Theme
  */
 
 get_header(); ?>
 
-<div class="front-background-image"> 
-  <h1>Shop Stuff</h1>
-  <h1>Inhabitent Journal</h1>
-</p>
+<div class='hero-banner'>
+
+  </div>
+
+  <h3>Shop Stuff</h3>
+
+<section class="taxonomy-sections">
+
+<?php
+  $terms = array( 'taxonomy' => 'product_type', 'hide_empty' => false);
+   $shop_stuff = get_terms( $terms );
+
+
+   foreach ( $shop_stuff as $term ): ?>
+   <div id="shopping">
+   
+   <img class="icon" src="<?php echo get_template_directory_uri() ?>/images/product-type-icons/<?php echo $term->name ?>.svg" alt="product type icons">
+     <p> <?php echo $term->description; ?> </p>
+     <a href='<?php echo get_term_link( $term ); ?>'>
+      <p class='button'> <?php echo $term->name; ?> Stuff </p>
+       </a>
 </div>
 
-  <?php
-   $args = array( 'post_type' => 'post', 'order' => 'ASC', 'numberposts' => 3 );
+ <?php endforeach; ?>
+
+</section>
+
+  <h3>Inhabitent Joural</h3>
+
+<section class="journals">
+
+<?php
+   $args = array( 'post_type' => 'post', 'order' => 'DESC', 'numberposts' => 3 );
    $journal_posts = get_posts( $args ); // returns an array of posts
 ?>
 <?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
       <div class='journal-post'>
-      <?php the_post_thumbnail();
-      inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' );?>
+      <?php the_post_thumbnail();?>
+      <?php inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' );?>
       <a href='<?php the_permalink(); ?>'>
       <h2><?php the_title(); ?></h2>
       </a>
-      <a class='read-entry' href='<?php the_permalink(); ?>'>READ ENTRY</a>
+      <a id='read-entry' href='<?php the_permalink(); ?>'>READ ENTRY</a>
 
       </div>
- 
-       <?php endforeach; wp_reset_postdata(); ?>
-     </div>
 
+<?php endforeach; wp_reset_postdata(); ?>
 
+</section>
 
+<h3>Latest Adventures</h3>
 
+<section class="boxone">
+  
+<div class="onex">
 
-<?php get_sidebar(); ?>
+<div class="one">
+  <a>
+<h2>Getting Back to Nature in a Canoe</h2>
+</a>
+<a id='read-entry1' href='<?php the_permalink(); ?>'>READ ENTRY</a>
+</div>
+
+<a id='tax-button1' href='<?php the_permalink(); ?>'>MORE ADVENTURES</a>
+</div>
+
+<div class="boxtwo">
+
+<div class="two">
+  <a>
+<h2>A Night with Friends at the Beach</h2>
+</a>
+<a id='read-entry1' href='<?php the_permalink(); ?>'>READ ENTRY</a>
+</div>
+
+<div class="three">
+  <a>
+<h2>Taking in the View at Big Mountain</h2>
+</a>
+<a id='read-entry1' href='<?php the_permalink(); ?>'>READ ENTRY</a>
+</div>
+
+<div class="four">
+  <a>
+<h2>Star-Gazing at the Night Sky</h2>
+</a>
+<a id='read-entry1' href='<?php the_permalink(); ?>'>READ ENTRY</a>
+</div>
+
+</div>
+
+</section>
+
 <?php get_footer(); ?>
+
+
+
+
+
 
